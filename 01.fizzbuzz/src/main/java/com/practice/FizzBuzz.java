322a4e89add16ee42280c8ed220654e3f62827d3
Yoan Thirion <yoan.thirion@gmail.com>
2024-11-22 07:53:37 +0100

    refactor: fizzbuzz

--- 01.fizzbuzz/src/main/java/com/practice/FizzBuzz.java
+++ 01.fizzbuzz/src/main/java/com/practice/FizzBuzz.java
@@ -1,23 +1,31 @@
 package com.practice;
 
 public class FizzBuzz {
+    public static final int MINIMUM = 1;
+    public static final int MAXIMUM = 100;
+
+    private FizzBuzz() {}
+
     public static String convert(int number) {
-        if (number < 1 || number > 100) {
-            throw new IllegalArgumentException("Number should be between 1 and 100");
-        }
+        checkValidInput(number);
+        return convertSafely(number);
+    }
 
-        if (number % 15 == 0) {
-            return "FizzBuzz";
-        }
-
-        if (number % 5 == 0) {
-            return "Buzz";
-        }
-
-        if (number % 3 == 0) {
-            return "Fizz";
-        }
+    private static String convertSafely(int number) {
+        if (isDivisibleBy(number, 15)) return "FizzBuzz";
+        if (isDivisibleBy(number, 5)) return "Buzz";
+        if (isDivisibleBy(number, 3)) return "Fizz";
 
         return String.valueOf(number);
+    }
+
+    private static boolean isDivisibleBy(int number, int x) {
+        return number % x == 0;
+    }
+
+    private static void checkValidInput(int number) {
+        if (number < MINIMUM || number > MAXIMUM) {
+            throw new IllegalArgumentException("Number should be between 1 and 100");
+        }
     }
 }
