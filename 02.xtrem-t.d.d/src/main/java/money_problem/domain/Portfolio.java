0e7e6ec0533340bdfd060f82f219e2ac783ac54b
Chams Eddine CISSE <chams-eddine-externe.cisse@edf.fr>
2024-12-05 08:10:59 +0100

    refactor: providing the Bank to Portfolio

--- 02.xtrem-t.d.d/src/main/java/money_problem/domain/Portfolio.java
+++ 02.xtrem-t.d.d/src/main/java/money_problem/domain/Portfolio.java
@@ -14,24 +14,23 @@ public class Portfolio {
         lines.put(currency, amount);
     }
 
-    public String evaluate(Currency currency) {
-        double sum = lines.entrySet()
-                .stream()
-                .map(entry -> convertToCurrency(currency, entry)
-                ).reduce(0.0, Double::sum);
-        return formatPortfolio(currency, sum);
-    }
-
     private static String formatPortfolio(Currency currency, double sum) {
         return sum + " " + currency;
     }
 
-    private static double convertToCurrency(Currency currency, Map.Entry<Currency, Double> entry) {
-        Bank bank = Bank.withExchangeRate(entry.getKey(), currency, 1.0);
+    private double convertToCurrency(Currency currency, Map.Entry<Currency, Double> entry, Bank bank) {
         try {
             return bank.convert(entry.getValue(), entry.getKey(), currency);
         } catch (MissingExchangeRateException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public String evaluate(Bank bank, Currency currency) {
+        double sum = lines.entrySet()
+                .stream()
+                .map(entry -> convertToCurrency(currency, entry, bank)
+                ).reduce(0.0, Double::sum);
+        return formatPortfolio(currency, sum);
     }
 }
--- 02.xtrem-t.d.d/src/test/java/money_problem/domain/PortfolioTest.java
+++ 02.xtrem-t.d.d/src/test/java/money_problem/domain/PortfolioTest.java
@@ -1,15 +1,30 @@
 package money_problem.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PortfolioTest {
     @Test
     void testPortfolioCanEvaluateWithinTheSameCurrency() {
+        Bank bank = Bank.withExchangeRate(Currency.USD, Currency.USD, 1.0);
         Portfolio portfolio = new Portfolio();
         portfolio.addMoney(5.0, Currency.USD);
         portfolio.addMoney(10.0, Currency.USD);
 
-        Assertions.assertThat(portfolio.evaluate(Currency.USD)).isEqualTo("15.0 USD");
+        Assertions.assertThat(portfolio.evaluate(bank, Currency.USD)).isEqualTo("15.0 USD");
+    }
+
+    @Test
+    @DisplayName("5 USD + 10 EUR = 17 USD")
+    void testEvaluateMultiCurrencies() {
+        Bank bank = Bank.withExchangeRate(Currency.USD, Currency.USD, 1.0);
+        bank.addExchangeRate(Currency.EUR, Currency.USD, 1.2);
+
+        Portfolio portfolio = new Portfolio();
+        portfolio.addMoney(5.0, Currency.USD);
+        portfolio.addMoney(10.0, Currency.EUR);
+
+        Assertions.assertThat(portfolio.evaluate(bank, Currency.USD)).isEqualTo("17.0 USD");
     }
 }
