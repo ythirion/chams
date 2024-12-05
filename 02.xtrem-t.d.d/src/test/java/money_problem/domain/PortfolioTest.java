ef33c1abe87eb92a1dd8855f803b4cb75d9824c1
Chams Eddine CISSE <chams-eddine-externe.cisse@edf.fr>
2024-12-05 08:12:46 +0100

    refactor: cleaning up the tests

--- 02.xtrem-t.d.d/src/test/java/money_problem/domain/PortfolioTest.java
+++ 02.xtrem-t.d.d/src/test/java/money_problem/domain/PortfolioTest.java
@@ -1,13 +1,21 @@
 package money_problem.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PortfolioTest {
+    private Bank bank;
+
+    @BeforeEach
+    void setUp() {
+        bank = Bank.withExchangeRate(Currency.USD, Currency.USD, 1.0);
+    }
+
     @Test
     void testPortfolioCanEvaluateWithinTheSameCurrency() {
-        Bank bank = Bank.withExchangeRate(Currency.USD, Currency.USD, 1.0);
+
         Portfolio portfolio = new Portfolio();
         portfolio.addMoney(5.0, Currency.USD);
         portfolio.addMoney(10.0, Currency.USD);
@@ -18,7 +26,6 @@ public class PortfolioTest {
     @Test
     @DisplayName("5 USD + 10 EUR = 17 USD")
     void testEvaluateMultiCurrencies() {
-        Bank bank = Bank.withExchangeRate(Currency.USD, Currency.USD, 1.0);
         bank.addExchangeRate(Currency.EUR, Currency.USD, 1.2);
 
         Portfolio portfolio = new Portfolio();
