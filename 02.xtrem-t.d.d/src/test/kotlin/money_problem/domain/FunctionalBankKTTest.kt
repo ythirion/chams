a55417d1a3993026fa4695a98f186f6ebe79b5db
Yoan Thirion <yoan.thirion@gmail.com>
2025-02-27 14:25:04 +0100

    fix: Remove useless tests

--- 02.xtrem-t.d.d/src/test/kotlin/money_problem/domain/FunctionalBankKTTest.kt
+++ 02.xtrem-t.d.d/src/test/kotlin/money_problem/domain/FunctionalBankKTTest.kt
@@ -10,7 +10,6 @@ import money_problem.domain.functional.MissingExchangeRateError
 import money_problem.domain.functional.Money
 
 class FunctionalBankKTTest : StringSpec({
-
     val bank = FunctionalBank.withExchangeRate(EUR, USD, 1.2)
 
     "10 EUR -> USD = 12 USD" {
@@ -21,23 +20,15 @@ class FunctionalBankKTTest : StringSpec({
         bank.convert(Money(10.0, EUR), EUR) shouldBe right(Money(10.0, EUR))
     }
 
-    "Should return a Left on missing exchange rate" {
+    "Return an error on missing exchange rate" {
         bank.convert(Money(10.0, EUR), KRW) shouldBe left(MissingExchangeRateError(EUR, KRW))
     }
 
     "Conversion with different exchange rates EUR to USD" {
         bank.convert(Money(10.0, EUR), USD) shouldBe right(Money(12.0, USD))
-        
+
         bank.addExchangeRate(EUR, USD, 1.3)
             .convert(Money(10.0, EUR), USD)
             .shouldBe(right(Money(13.0, USD)))
-    }
-
-    "Should return an Either when convert is called" {
-        bank.convert(Money(10.0, EUR), KRW) shouldBe left(MissingExchangeRateError(EUR, KRW))
-    }
-
-    "Should convert currency without error with result" {
-        bank.convert(Money(10.0, EUR), USD) shouldBe right(Money(12.0, USD))
     }
 })
