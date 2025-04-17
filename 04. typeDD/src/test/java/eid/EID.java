528fc5a312e1ba3c41c27ef4757e51d265042503
Yoan Thirion <yoan.thirion@gmail.com>
2025-04-17 12:56:48 +0200

    test: add Property for day17

---
+++ 04. typeDD/src/test/java/eid/EID.java
@@ -0,0 +1,9 @@
+package eid;
+
+import io.vavr.control.Either;
+
+public class EID {
+    public static Either<ParsingError, EID> parse(String potentialEID) {
+        return null;
+    }
+}
---
+++ 04. typeDD/src/test/java/eid/EIDProperties.java
@@ -0,0 +1,20 @@
+package eid;
+
+import io.vavr.test.Arbitrary;
+import io.vavr.test.Property;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EIDProperties {
+    private final Arbitrary<EID> validEID = null;
+
+    @Test
+    void roundTrip() {
+        Property.def("parseEID(eid.toString) == eid")
+                .forAll(validEID)
+                .suchThat(eid -> EID.parse(eid.toString()).contains(eid))
+                .check()
+                .assertIsSatisfied();
+    }
+}
